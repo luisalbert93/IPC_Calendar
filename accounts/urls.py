61af5d1597edd75e668077d587a97a31e302f7fd
Luis <luisdonis777@gmail.com>
2023-02-16 21:47:52 -0800

    setting up Flake 8

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('signin/', views.SignInView.as_view(), name='signin'),
     path('signout/', views.signout, name='signout'),
+]
