b4649ae244dd02fe6015ab1697ad200ac0eedfe6
Luis <luisdonis777@gmail.com>
2023-02-19 13:33:27 -0800

    url change

--- eventcalendar/views.py
+++ eventcalendar/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from calendar.forms import SignupForm
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 
-def signup(request):
-    forms = SignupForm()
-    if request.method == 'POST':
-        forms = SignupForm(request.POST)
-        if forms.is_valid():
-            username = forms.cleaned_data['username']
-            password = forms.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                return redirect('calendarapp:calendar')
-    context = {'form': forms}
-    return render(request, 'signup.html', context)
+from calendarapp.models import Event
 
 
-def user_logout(request):
-    logout(request)
-    return redirect('signup')
+class DashboardView(LoginRequiredMixin, View):
+    login_url = "accounts:signin"
+    template_name = "calendarapp/dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        events = Event.objects.get_all_events(user=request.user)
+        running_events = Event.objects.get_running_events(user=request.user)
+        latest_events = Event.objects.filter(user=request.user).order_by("-id")[:10]
+        context = {
+            "total_event": events.count(),
+            "running_events": running_events,
+            "latest_events": latest_events,
+        }
+        return render(request, self.template_name, context)
