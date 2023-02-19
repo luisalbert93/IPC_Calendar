6b32552003c822e507b1273ad134eed4140ea6ce
Luis <luisdonis777@gmail.com>
2023-02-19 13:28:13 -0800

    modyfing template

--- calendar/forms.py
+++ calendar/forms.py
@@ -1,30 +1,36 @@
 from django.forms import ModelForm, DateInput
-from calendar.models import Event, EventMember
+from calendarapp.models import Event
 from django import forms
 
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
+        fields = ["title", "description", "start_time", "end_time"]
         # datetime-local is a HTML5 input type
         widgets = {
-            'start_time': DateInput(
-                attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'
+            "title": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter event title"}
             ),
-            'end_time': DateInput(
-                attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter event description",
+                }
+            ),
+            "start_time": DateInput(
+                attrs={"type": "datetime-local", "class": "form-control"},
+                format="%Y-%m-%dT%H:%M",
+            ),
+            "end_time": DateInput(
+                attrs={"type": "datetime-local", "class": "form-control"},
+                format="%Y-%m-%dT%H:%M",
             ),
         }
-        exclude = ['user']
+        exclude = ["user"]
 
     def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
         # input_formats to parse HTML5 datetime-local input to datetime field
-        self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-        self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-
-
-class AddMemberForm(forms.ModelForm):
-    class Meta:
-        model = EventMember
-        fields = ['user']
+        self.fields["start_time"].input_formats = ("%Y-%m-%dT%H:%M",)
+        self.fields["end_time"].input_formats = ("%Y-%m-%dT%H:%M",)
--- calendar/urls.py
+++ calendar/urls.py
@@ -2,27 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'calendarapp'
-
+app_name = "calendarapp"
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('', views.DashboardView.as_view(), name='dashboard'),
-    path('calender/', views.CalendarView.as_view(), name='calendar'),
-    path('event/new/', views.create_event, name='event_new'),
-    path(
-        'event/edit/<int:pk>/', views.EventEdit.as_view(), name='event_edit'
-    ),
-    path(
-        'event/<int:event_id>/details/', views.event_details,
-        name='event-detail'
-    ),
-    path(
-        'add_eventmember/<int:event_id>', views.add_eventmember,
-        name='add_eventmember'
-    ),
-    path(
-        'event/<int:pk>/remove', views.EventMemberDeleteView.as_view(),
-        name="remove_event"
-    ),
+    path("calendar/", views.CalendarViewNew.as_view(), name="calendar"),
+    path("event/new/", views.create_event, name="event_new"),
+    path("all-event-list/", views.AllEventsListView.as_view(), name="all_events"),
+    # path("event/<int:id>/remove/", views.remove_event, name="event_remove")
+    path("event/remove/", views.remove_event, name="event_remove")
 ]
